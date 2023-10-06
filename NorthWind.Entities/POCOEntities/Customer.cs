2f249a4b8865c70504d0a88f475b2d6933b1c2a7
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-10-06 14:40:37 -0500

    Clase Order

--- NorthWind.Entities/POCOEntities/Customer.cs
+++ NorthWind.Entities/POCOEntities/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NorthWind.Entities.POCOEntities
 {
-    internal class Customer
+    public class Customer
     {
         public string Id { get; set; }
         public string Name { get; set; }
