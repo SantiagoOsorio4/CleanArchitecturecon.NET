b8b743d8c90fa64fd2d826a058ab93539615f1cb
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-10-06 16:18:18 -0500

    Migrations

--- NorthWind.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
+++ NorthWind.Repositories.EFCore/DataContext/NorthWindContextFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace NorthWind.Repositories.EFCore.DataContext
 {
-    internal class NorthWindContextFactory
+     class NorthWindContextFactory :
+        IDesignTimeDbContextFactory<NorthWindContext>
     {
+        public NorthWindContext CreateDbContext(string[] args)
+        {
+            var OptionBuilder =
+                new DbContextOptionsBuilder<NorthWindContext>();
+            OptionBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;database=NorthWindDB");
+            return new NorthWindContext(OptionBuilder.Options);
+        }
+
     }
 }
