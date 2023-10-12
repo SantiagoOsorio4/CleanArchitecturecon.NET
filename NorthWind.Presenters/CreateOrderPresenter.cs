5024c0cb5d9914f53aceee2098448b7ff9a38e14
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-10-12 17:36:12 -0500

    CreateOrder

---
+++ NorthWind.Presenters/CreateOrderPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NorthWind.Presenters
+{
+    public class CreateOrderPresenter : IPresenter<int, string>
+    {
+        public string Content {get; private set;}
+
+        public void Handle(int response)
+        {
+            Content = $"Order ID: {response}";
+        }
+    }
+}
