5687caba474401f1a52a74d1065d3d31b8e7e991
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-10-12 17:24:40 -0500

    Modificacion a CrateOrder

--- NorthWind.UseCases/CreateOrder/CreateOrderInteractor.cs
+++ NorthWind.UseCases/CreateOrder/CreateOrderInteractor.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace NorthWind.UseCases.CreateOrder
 {
-    public class CreateOrderInteractor : IRequestHandler<CreateOrderInputPort, int>
+    public class CreateOrderInteractor : AsyncRequestHandler<CreateOrderInputPort>
     {
         readonly IOrderRepository OrderRepository;
         readonly IOrderDetailRepository OrderDetailRepository;
@@ -20,22 +20,22 @@ namespace NorthWind.UseCases.CreateOrder
             (orderRepository, orderDetailRepository, unitOfWork);
 
 
-        public async Task<int> Handle(CreateOrderInputPort request, CancellationToken cancellationToken)
+        protected async override Task Handle(CreateOrderInputPort request, CancellationToken cancellationToken)
         {
             Order Order = new Order()
             {
-                CustomerId = request.CustomerId,
+                CustomerId = request.RequestData.CustomerId,
                 OrderDate = DateTime.Now,
-                ShipAddress = request.ShipAddress,
-                ShipCity = request.ShipCity,
-                ShipCountry = request.ShipCountry,
-                ShipPostalCode = request.ShipPotalCode,
+                ShipAddress = request.RequestData.ShipAddress,
+                ShipCity = request.RequestData.ShipCity,
+                ShipCountry = request.RequestData.ShipCountry,
+                ShipPostalCode = request.RequestData.ShipPotalCode,
                 ShippingType = Entities.Enums.ShippingType.Road,
                 DiscountType = Entities.Enums.DiscountType.Percentage,
                 Discount = 10
             };
             OrderRepository.Create(Order);
-            foreach(var Item in request.OrderDetails)
+            foreach(var Item in request.RequestData.OrderDetails)
             {
                 OrderDetailRepository.Create(
                     new OrderDetail
@@ -52,9 +52,10 @@ namespace NorthWind.UseCases.CreateOrder
             }
             catch (Exception ex) 
             {
-                throw new GeneralException("Error al crear la orden.", ex.Message);
+                throw new GeneralException("Error al crear la orden.", 
+                    ex.Message);
             }
-            return Order.Id;
+            request.OutputPort.Handle(Order.Id);
         }
     }
 }
