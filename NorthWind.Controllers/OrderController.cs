20d81ad918f94d3761052ea86b43c36eba3e2278
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-10-12 21:00:31 -0500

    Controllers

---
+++ NorthWind.Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NorthWind.Presenters;
+using NorthWind.UseCasesDTOs.CreateOrder;
+
+namespace NorthWind.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class OrderController
+    {
+        readonly IMediator Mediator;
+        public OrderController(IMediator mediator) =>
+            Mediator = mediator;
+
+        [HttpPost("create-order")]
+        public async Task<string> CreateOrder(
+            CreateOrderParams orderparams)
+        {
+            CreateOrderPresenter
+        }
+
+    }
+}
